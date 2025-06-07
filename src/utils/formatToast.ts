aca32df2b117099e1c8610cdda39b90e93176a5b
cesaredm <cesaredmaradiaga@outlook.com>
2025-06-07 17:34:52 -0600

    Utilidad de formatos de Toast

---
+++ src/utils/formatToast.ts
@@ -0,0 +1,13 @@
+import { isAxiosError } from "axios";
+import { ToastMessageOptions } from "primereact/toast";
+export function toastSuccess(data: unknown): ToastMessageOptions {
+  return { ...(data as ToastMessageOptions), life: 3000 };
+}
+export function toastError(data: unknown): ToastMessageOptions {
+  if (isAxiosError(data)) {
+    console.log(data?.response);
+    return { severity: data.response?.data.severity, summary: data.response?.data.summary, detail: data.response?.data.detail, life: 3000 };
+  } else {
+    return { severity: "error", summary: "Error", detail: data, life: 3000 };
+  }
+}
